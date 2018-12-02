043a9a05dac1a3c0d6ddb8c5ce3ad726f91b22f4
Rohan <rhnmht30@gmail.com>
2018-12-02 20:06:47 +0530

    Final Changes

--- myApp/static/where.js
+++ myApp/static/where.js
@@ -1,8 +1,4 @@
 myData = [
-[25.3176452,82.9739144, 'Varanasi, Uttar Pradesh, India'],
-[25.2048493,55.2707828, 'Dubai - United Arab Emirates'],
-[26.449923,80.3318736, 'Kanpur, Uttar Pradesh, India'],
-[37.09024,-95.712891, 'United States'],
-[19.0759837,72.8776559, 'Mumbai, Maharashtra, India'],
-[36.204824,138.252924, 'Japan']
+[27.1750151,78.0421552, 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India'],
+[25.3176452,82.9739144, 'Varanasi, Uttar Pradesh, India']
 ];
