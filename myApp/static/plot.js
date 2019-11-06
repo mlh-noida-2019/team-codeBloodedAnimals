c6a1562ef0e64b85a814bbce3c348125de0133f9
Rohan <rhnmht30@gmail.com>
2019-11-06 15:43:26 +0530

    add all rem files

--- myApp/static/plot.js
+++ myApp/static/plot.js
@@ -1,3 +1,6 @@
 myData = [
-[27.1750151,78.0421552, 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India']
+[27.1750151,78.0421552, 'Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India'],
+[19.1095171,72.8241298, 'Juhu Beach, Juhu, Mumbai, Maharashtra 400049, India'],
+[25.3176452,82.9739144, 'Varanasi, Uttar Pradesh, India'],
+[25.3176452,82.9739144, 'Varanasi, Uttar Pradesh, India']
 ];
